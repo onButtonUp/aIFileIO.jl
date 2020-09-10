f28a6a4d94bf600316c8f931994e58342b808631
onButtonUp <jtferrier3@msn.com>
2020-09-09 20:49:27 -0700

    boom
    .

--- src/aIFileIO.jl
+++ src/aIFileIO.jl
@@ -4,7 +4,7 @@ usingDates, CSV, DataFrames
 
 include("AIFileFunctions.jl")
 
-export log_file, read_log, count_log_events, , read_sourceFolder
+export log_file, read_log, count_log_events, read_sourceFolder
 export process_sourceFolder, count_CSV_files, count_TXT_files, set_sourcePath, show_sourcePath
 export show_CSV_files, show_TXT_files
 
