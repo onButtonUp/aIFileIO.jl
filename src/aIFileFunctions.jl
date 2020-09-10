195520bb29aa3aaf1e98f8733238646ccee3a9bd
onButtonUp <jtferrier3@msn.com>
2020-09-09 20:40:33 -0700

    Push . . .
    it.

---
+++ src/aIFileFunctions.jl
@@ -0,0 +1,75 @@
+function log_file(thisMessage::String, returnValue::Int = 0)
+## when the log file is first created use append = false . . .
+    #    CSV.write("logFile.csv", DataFrame(time = [get_time()], event = [thisMessage], retcode = [returnValue]), append=false)
+  if true == taskLoggingOn
+    CSV.write("logFile.csv", DataFrame(time = [get_time()], event = [thisMessage], retcode = [returnValue]), append=true)
+  end
+end
+
+function read_log()
+    logDF = DataFrame()
+    logDF = CSV.File("logFile.csv") |> DataFrame
+end
+
+function count_log_events()
+    logDF = DataFrame()
+    logDF = CSV.File("logFile.csv")
+    return size(logDF)[1]
+end
+
+function process_sourceFolder()
+  sourceFiles = read_sourceFolder()
+  for sf ∈ sourceFiles
+    
+    println("file: $sf")
+  end
+end
+
+function count_CSV_files()
+
+  sourceFiles = read_sourceFolder()
+ 
+  occursin.(r".csv", sourceFiles) |> 
+  collect |> 
+  sum
+
+end
+
+function count_TXT_files()
+
+  sourceFiles = read_sourceFolder()
+ 
+  occursin.(r".txt", sourceFiles) |> 
+  collect |> 
+  sum
+
+end
+
+function show_CSV_files()
+
+  sourceFiles = read_sourceFolder()
+ 
+  return occursin.(r".csv", sourceFiles) 
+
+end
+
+
+function show_TXT_files()
+
+  sourceFiles = read_sourceFolder()
+ 
+  return occursin.(r".txt", sourceFiles)
+
+end
+
+function read_sourceFolder()
+    readdir(taskSourcePath)
+end
+
+function set_sourcePath(thisPath::String)
+    global taskSourcePath = thisPath
+end
+
+function show_sourcePath()
+    return taskSourcePath
+end
--- src/aIFileIO.jl
+++ src/aIFileIO.jl
@@ -1,5 +1,11 @@
 module aIFileIO
 
-# Write your package code here.
+usingDates, CSV, DataFrames
+
+include("AIFileFunctions.jl")
+
+export log_file, read_log, count_log_events, , read_sourceFolder
+export process_sourceFolder, count_CSV_files, count_TXT_files, set_sourcePath, show_sourcePath
+export show_CSV_files, show_TXT_files
 
 end
